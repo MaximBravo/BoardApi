3a53256ee4e48ba64f64afa6846c53da6c11f5fc
Maxim Bravo <maxim@alexbravo.com>
2016-12-17 16:23:29 -0800

    Implement showing if screen was touched

--- app/src/main/java/com/example/maximbravo/boardapi/MainActivity.java
+++ app/src/main/java/com/example/maximbravo/boardapi/MainActivity.java
@@ -5,17 +5,22 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    //Textview representing output textview
+    private TextView output;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //initialize output textview
+        output = (TextView) findViewById(R.id.output_textview);
 
         //fab stuff
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -33,7 +38,18 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //if finger is on screen or not
+        if(event.getAction() != MotionEvent.ACTION_UP){
+            //set output text if touching the screen
+            output.setText("You are touching the screen! :)");
+        } else {
+            //set output text if not touching the screen
+            output.setText("You are not touching the screen! :(");
+        }
+        return super.onTouchEvent(event);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
